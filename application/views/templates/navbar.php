6c99ea92d239f98906e71fbc0a8b4147998b4425
Maghin <maghin@devcentos7.merhylstudio.org>
2016-11-11 17:45:38 +0100

    site base

---
+++ application/views/templates/navbar.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<nav class="container-fluid navbar navbar-default navbar-fixed-top">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="http://www.merhylstudio.org">
+      <img class="img-responsive logo" src="assets/img/logo64.png" alt="MerhylStudio" />
+      <span class="hidden-xs">MerhylStudio</span></a>
+  </div>
+  <div class="collapse navbar-collapse" id="navbar">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Flow</a></li>
+      <li><a href="#">Server</a></li>
+      <li><a href="#">Tools</a></li>
+      <li><a href="#">Help</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>

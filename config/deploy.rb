ff41f163dc2d4800f95e39564f93dd1c65dfa5d7
Maghin <maghin@devcentos7.merhylstudio.org>
2016-11-12 18:35:10 +0100

    Configure deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,39 @@
-# config valid only for current version of Capistrano
+# Config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# Set the log level
+set :log_level, :debug
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Remote server and website common parameters
+set :application, "merhylstudio.org"
+set :keep_releases, 5
+set :deploy_to, "/app/httpd/www/#{fetch(:application)}"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# Git parameters with URL to repository (can be local)
+set :scm, :git
+set :repo_url, 'https://git@github.com/merhylstudio/merhylstudio.org.git'
+#ask :branch, 'git rev-parse --abbrev-ref HEAD'.chomp
+set :branch, 'master'
 
-# Default value for :scm is :git
-# set :scm, :git
+# Linked in shared folder
+append :linked_files, "application/config/database.php"
+append :linked_dirs, "application/logs", "application/cache"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# Chemin de composer (shared_path)
+SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+# debug sur composer
+set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
 
-# Default value for :pty is false
-# set :pty, true
+set :pty, false
 
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+#############################################################
+## FLOW
+##
+namespace :deploy do
+  # Installing composer (in shared_path by default)
+  after :starting, 'composer:install_executable'
 
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  # Finishing tasks
+  after :finishing, 'deploy:cleanup'
+end
